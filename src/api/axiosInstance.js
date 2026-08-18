5ced7d7110badc1be24e1b2cb7308db24df35906
SOORAJ RAJU <soorajraju826@gmail.com>
2026-08-18 20:39:32 +0530

    Connect React to deployed API

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://product-order-dashboard-1.onrender.com"
 });
 
 export default axiosInstance;
